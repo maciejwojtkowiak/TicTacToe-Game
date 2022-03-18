04c13ae73ba310b871f85b4d7dc162de25b35db9
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-03-18 19:54:55 +0100

    created interface for chosen tiles numbers and created helper method to change players

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,26 @@
 "use strict";
+var ActivePlayer;
+(function (ActivePlayer) {
+    ActivePlayer[ActivePlayer["X"] = 0] = "X";
+    ActivePlayer[ActivePlayer["O"] = 1] = "O";
+})(ActivePlayer || (ActivePlayer = {}));
 class Board {
     constructor() {
         this.boardTiles = 9;
         this.rootContainer = document.querySelector(".root");
         this.players = ["X", "O"];
+        this.active = ActivePlayer.O;
+        this.cells = document.querySelectorAll("[data-number]");
+        this.winningConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
         this.buildTiles();
         this.onTileClick();
     }
@@ -11,6 +28,7 @@ class Board {
         for (let i = 0; i < this.boardTiles; i++) {
             let tile = document.createElement("div");
             tile.dataset.number = i.toString();
+            tile.classList.add("tile");
             tile.innerText = "hi";
             tile.style.height = "100px";
             tile.style.width = "100px";
@@ -20,8 +38,20 @@ class Board {
     }
     onTileClick() {
         this.rootContainer.addEventListener("click", (e) => {
-            console.log(e.target);
+            const tile = e.target;
+            if (tile.classList.contains("tile")) {
+                console.log(tile);
+                console.log(this.cells);
+                if (this.active === ActivePlayer.O) {
+                    tile.style.backgroundColor = "green";
+                    this.winningConditions.some((combination) => combination.every((index) => console.log(index)));
+                }
+                if (this.active === ActivePlayer.X) {
+                    tile.style.backgroundColor = "black";
+                }
+            }
         });
     }
+    changePlayer() { }
 }
 const TicTacToeBoard = new Board();
--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,26 @@ enum ActivePlayer {
   O,
 }
 
+interface ChosenNumbers {
+  O: string[];
+  X: string[];
+}
+
 class Board {
   private boardTiles = 9;
-
   private rootContainer = document.querySelector(".root") as HTMLDivElement;
-  private players = ["X", "O"];
-  private Active = ActivePlayer.O;
+  private active = ActivePlayer.O;
+  private chosenNumbers: ChosenNumbers = { O: [], X: [] };
+  private winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   constructor() {
     this.buildTiles();
@@ -19,6 +33,7 @@ class Board {
     for (let i = 0; i < this.boardTiles; i++) {
       let tile = document.createElement("div");
       tile.dataset.number = i.toString();
+      tile.classList.add("tile");
       tile.innerText = "hi";
       tile.style.height = "100px";
       tile.style.width = "100px";
@@ -28,13 +43,29 @@ class Board {
   }
 
   onTileClick() {
-    if (this.Active === ActivePlayer.O)
-      this.rootContainer.addEventListener("click", (e) => {
-        console.log(e.target);
-      });
-    if (this.Active === ActivePlayer.X) {
-      console.log("X");
-    }
+    this.rootContainer.addEventListener("click", (e: Event) => {
+      const tile = e.target as HTMLDivElement;
+      if (tile.classList.contains("tile")) {
+        if (this.active === ActivePlayer.O) {
+          tile.style.backgroundColor = "green";
+          const dataNumber = tile.getAttribute("data-number");
+          this.chosenNumbers.O.push(dataNumber!);
+          this.winningConditions.some((combination) =>
+            combination.every((index) => console.log(index))
+          );
+          this.changePlayer();
+        }
+        if (this.active === ActivePlayer.X) {
+          tile.style.backgroundColor = "black";
+          this.changePlayer();
+        }
+      }
+    });
+  }
+
+  changePlayer() {
+    if (this.active === ActivePlayer.X) this.active = ActivePlayer.O;
+    if (this.active === ActivePlayer.O) this.active = ActivePlayer.X;
   }
 }
 
